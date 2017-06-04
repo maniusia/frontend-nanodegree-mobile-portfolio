45d5df98d85648a9e8b8fef79814536fe85853f1
maniusia <monia.kum@icloud.com>
2017-06-05 00:01:53 +0100

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -418,33 +418,25 @@ var resizePizzas = function(size) {
   changeSliderLabel(size);
 
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
 
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
-    }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    switch (size) {
+      case '1':
+      newWidth = 25;
+        break;
+        case '2':
+        newWidth = 33.3;
+          break;
+          case '3':
+          newWidth = 50;
+            break;
+            default:
+            console.log("ups, sizeSwitcher doesn't work");
+
+
+    }
     var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
     for (var i = 0; i < randomPizzas.length; i++) {
       randomPizzas[i].style.width = newWidth + "%";
